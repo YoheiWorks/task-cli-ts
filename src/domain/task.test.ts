c89c3df31502603cb1b473eb303dfbca49601115
橋本洋平 <y.hashimoto.enmore.engineer@gmail.com>
2026-03-25 12:35:28 +0900

    test: createTask関数の2つ目のテスト作成
    docs/test/テスト観点整理.md参照

--- src/domain/task.test.ts
+++ src/domain/task.test.ts
@@ -13,5 +13,17 @@ describe("タスクの生成に関するテスト", () => {
             expect(result.value.title).toBe(title);
             expect(result.value.status).toBe("open");
         }
-    })
-})
+    });
+
+    it ("1文字のタイトルでタスクを生成できる", () => {
+        const title = "n";
+        const result = createTask(title);
+
+        expect(result.kind).toBe("ok");
+        if (result.kind === "ok") {
+            expect(result.value.id.length).toBeGreaterThan(0);
+            expect(result.value.title).toBe(title);
+            expect(result.value.status).toBe("open");
+        }
+    });
+});
