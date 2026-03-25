3c9973311eac7855e9fd50017ab2ef4a84c1f27f
橋本洋平 <y.hashimoto.enmore.engineer@gmail.com>
2026-03-25 12:38:37 +0900

    test: createTask関数に対する全てのテスト作成
    docs/test/テスト観点整理.md参照

--- src/domain/task.test.ts
+++ src/domain/task.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "vitest";
-import { createTask } from "./task.js";
+import { createTask, InvalidTaskTitleError } from "./task.js";
 
 describe("タスクの生成に関するテスト", () => {
     it ("複数文字数のタイトルでタスクを生成できる", () => {
@@ -24,6 +24,36 @@ describe("タスクの生成に関するテスト", () => {
             expect(result.value.id.length).toBeGreaterThan(0);
             expect(result.value.title).toBe(title);
             expect(result.value.status).toBe("open");
+        }
+    });
+
+    it ("空文字のタイトルでタスクを生成できない", () => {
+        const title = "";
+        const result = createTask(title);
+
+        expect(result.kind).toBe("error");
+        if (result.kind === "error") {
+            expect(result.error).toBeInstanceOf(InvalidTaskTitleError);
+        }
+    });
+
+    it ("半角スペースのみのタイトルでタスクを生成できない", () => {
+        const title = " ";
+        const result = createTask(title);
+
+        expect(result.kind).toBe("error");
+        if (result.kind === "error") {
+            expect(result.error).toBeInstanceOf(InvalidTaskTitleError);
+        }
+    });
+
+    it ("全角スペースのみのタイトルでタスクを生成できない", () => {
+        const title = "　";
+        const result = createTask(title);
+
+        expect(result.kind).toBe("error");
+        if (result.kind === "error") {
+            expect(result.error).toBeInstanceOf(InvalidTaskTitleError);
         }
     });
 });
