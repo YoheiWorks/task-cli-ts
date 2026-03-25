ccefbb0b55b04e94c8e37306cf14b26ff9f9dc12
橋本洋平 <y.hashimoto.enmore.engineer@gmail.com>
2026-03-25 12:33:43 +0900

    test: createTask関数の1つ目のテスト作成
    docs/テスト観点整理.mdを参照

--- src/domain/task.test.ts
+++ src/domain/task.test.ts
@@ -1,7 +1,17 @@
 import { describe, expect, it } from "vitest";
+import { createTask } from "./task.js";
 
-describe("test", () => {
-    it ("should work", () => {
-        expect(1 + 1).toBe(2);
+describe("タスクの生成に関するテスト", () => {
+    it ("複数文字数のタイトルでタスクを生成できる", () => {
+        const title = "new Task";
+
+        const result = createTask(title);
+        
+        expect(result.kind).toBe("ok");
+        if (result.kind === "ok") {
+            expect(result.value.id.length).toBeGreaterThan(0);
+            expect(result.value.title).toBe(title);
+            expect(result.value.status).toBe("open");
+        }
     })
 })
