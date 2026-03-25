576a43d5f2ce566ea2141257c0b9c94c7eb4f675
橋本洋平 <y.hashimoto.enmore.engineer@gmail.com>
2026-03-25 10:46:09 +0900

    feat: Task生成時の処理を修正
    1. Taskのタイトルについてバリデーション関数を作成
    2. Result型を作成
    3. ドメインエラー型を作成
    4. createTask関数をバリデーション関数とResult型とドメインエラー型を使うように修正

--- src/domain/task.ts
+++ src/domain/task.ts
@@ -10,12 +10,40 @@ type TaskTitle = string;
 
 type TaskStatus = "open" | "completed";
 
-function createTask(title: TaskTitle): Task {
-    return {
-        id: crypto.randomUUID(),
-        title,
-        status: "open",
+type Result<T, E extends Error> = 
+    { readonly kind: "ok"; readonly value: T}
+    | { readonly kind: "error"; readonly error: E };
+
+class InvalidTaskTitleError extends Error {
+    constructor() {
+        super("タイトルが空文字・空白のみでは、タスクを作成できません。");
+        this.name = "InvalidTaskTitleError";
+
+        Object.setPrototypeOf(this, InvalidTaskTitleError.prototype);
     }
 }
 
-export { type Task, type TaskId, type TaskTitle, type TaskStatus, createTask };
+function createTask(title: TaskTitle): Result<Task, InvalidTaskTitleError> {
+
+    if (!validateTaskTitle(title)) {
+        return {
+            kind: "error",
+            error: new InvalidTaskTitleError()
+        };
+    }
+
+    return {
+        kind: "ok",
+        value: {
+            id: crypto.randomUUID(),
+            title,
+            status: "open"
+        }
+    };
+}
+
+function validateTaskTitle(title: TaskTitle): boolean {
+    return title.trim().length > 0;
+}
+
+export { type Task, type TaskId, type TaskTitle, type TaskStatus, type Result, type InvalidTaskTitleError, createTask };
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
 import { createTask } from "./domain/task.js";
 
-const task = createTask("新しいタスク");
-console.log(task);
+const task = createTask(" f ");
+switch (task.kind) {
+    case "ok":
+        console.log("タスクが作成されました。");
+        console.log(`タスク: ${task.value.title} (ID: ${task.value.id}, Status: ${task.value.status})`);
+        break;
+    case "error":
+        console.error(`タスクの作成に失敗しました: \n ${task.error.name}:${task.error.message}`);
+        break;
+}
